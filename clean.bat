74b7bdb0c04e9116ae30201c9be41d3f01e5072b
Rubin Simons <rubin@raaftech.com>
2017-01-26 15:45:22 +0100

    cleanup executable

--- clean.bat
+++ clean.bat
@@ -2,6 +2,6 @@
 del cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb >nul 2>&1
 rd /s/q CMakeFiles Debug x64 >nul 2>&1
 
-del hello.c >nul 2>&1
+del hello.c hello.exe >nul 2>&1
 rd /s/q hello.dir >nul 2>&1
 
