421f7e77ccea3ec7e7b5293245fac12398cd7d09
Rubin Simons <rubin@raaftech.com>
2017-01-27 14:59:40 +0100

    Added a few more files to clean.

--- clean.bat
+++ clean.bat
@@ -1,5 +1,5 @@
 @echo off
-del cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb CPackConfig.cmake CPackSourceConfig.cmake CTestTestfile.cmake >nul 2>&1
+del install_manifest.txt cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb CPackConfig.cmake CPackSourceConfig.cmake CTestTestfile.cmake >nul 2>&1
 rd /s/q _CPack_Packages CMakeFiles Debug Testing x64 >nul 2>&1
 
 del hello.c hello.exe >nul 2>&1
--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -f cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb CPackConfig.cmake CPackSourceConfig.cmake CTestTestfile.cmake
+rm -f install_manifest.txt cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb CPackConfig.cmake CPackSourceConfig.cmake CTestTestfile.cmake
 rm -rf _CPack_Packages CMakeFiles Debug Testing x64
 
 rm -f hello.c
