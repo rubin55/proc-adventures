95397017ed798d68ffee1e7f0ca52c23f4b78ade
Rubin Simons <rubin@raaftech.com>
2017-01-27 14:29:30 +0100

    More excludes and cleanup.

--- clean.bat
+++ clean.bat
@@ -1,6 +1,6 @@
 @echo off
-del cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb >nul 2>&1
-rd /s/q CMakeFiles Debug x64 >nul 2>&1
+del cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb CPackConfig.cmake CPackSourceConfig.cmake CTestTestfile.cmake >nul 2>&1
+rd /s/q _CPack_Packages CMakeFiles Debug Testing x64 >nul 2>&1
 
 del hello.c hello.exe >nul 2>&1
 rd /s/q hello.dir >nul 2>&1
