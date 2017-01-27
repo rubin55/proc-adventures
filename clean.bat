c5aa1fa0268e1ac2e96fdc9137230606bad7ffec
Rubin Simons <rubin@raaftech.com>
2017-01-27 15:23:42 +0100

    More visual studio cleanup.

--- clean.bat
+++ clean.bat
@@ -1,6 +1,6 @@
 @echo off
-del install_manifest.txt cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb CPackConfig.cmake CPackSourceConfig.cmake CTestTestfile.cmake >nul 2>&1
-rd /s/q _CPack_Packages CMakeFiles Debug Testing x64 >nul 2>&1
+del specs.i proc-adventures.VC.db install_manifest.txt cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb CPackConfig.cmake CPackSourceConfig.cmake CTestTestfile.cmake >nul 2>&1
+rd /s/q _CPack_Packages CMakeFiles Debug Testing x64 .settings .vs >nul 2>&1
 
 del hello.c hello.exe >nul 2>&1
 rd /s/q hello.dir >nul 2>&1
--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-rm -f install_manifest.txt cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb CPackConfig.cmake CPackSourceConfig.cmake CTestTestfile.cmake
-rm -rf _CPack_Packages CMakeFiles Debug Testing x64
+rm -f specs.i proc-adventures.VC.db install_manifest.txt cmake_install.cmake CMakeCache.txt *.vcxproj *.vcxproj.filters *.sln .cproject .project Makefile *.ilk *.pdb CPackConfig.cmake CPackSourceConfig.cmake CTestTestfile.cmake
+rm -rf _CPack_Packages CMakeFiles Debug Testing x64 .settings .vs
 
 rm -f hello.c
 rm -rf hello.dir
